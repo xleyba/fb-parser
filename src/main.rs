be56eb9c08aefa08b34ddf0491c1212c8f48e2da
Javier L <135663+xleyba@users.noreply.github.com>
2019-12-21 21:48:30 +0100

    added structure

--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,8 @@ pub struct Photo {
     uri: String,
     creation_timestamp: u64,
     media_metadata: PhotoMetaData,
+    #[serde(default)]
+    comments: Vec<Comment>,
     title: String,
 }
 
@@ -33,11 +35,11 @@ pub struct PhotoMetaData {
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Metadata {
     #[serde(default)]
-    taken_timestamp: String,
+    taken_timestamp: u64,
     #[serde(default)]
-    modified_timestamp: String,
+    modified_timestamp: u64,
     #[serde(default)]
-    creation_timestamp: String,
+    creation_timestamp: u64,
     #[serde(default)]
     f_stop: String,
     #[serde(default)]
@@ -45,24 +47,33 @@ pub struct Metadata {
     #[serde(default)]
     camera_model: String,
     #[serde(default)]
-    latitude: String,
+    latitude: f64,
     #[serde(default)]
-    longitude: String,
+    longitude: f64,
     #[serde(default)]
     exposure: String,
     #[serde(default)]
-    iso_speed: String,
+    iso_speed: u64,
     #[serde(default)]
     focal_length: String,
     #[serde(default)]
     upload_ip: String,
 }
 
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Comment {
+    #[serde(default)]
+    timestamp: u64,
+    #[serde(default)]
+    comment: String,
+    #[serde(default)]
+    author: String,
+}
+
 #[derive(StructOpt, Debug)]
 struct Opt {
-    
-    /// This option is positional, meaning it is the first unadorned string
-    /// you provide (multiple others could follow).
+
+    #[structopt(value_name = "FILE", help = "The JSON file to be parsed")]
     jfile: String,
 }
 
@@ -71,26 +82,42 @@ fn main() {
 
     let opt = Opt::from_args();
 
-    println!("{:?}", opt);
+    let mut cmd_line = "".to_string();
     
 
-    let file = File::open("G:\\photos_and_videos\\album\\0.json").expect("File not found");
+    //let file = File::open("G:\\photos_and_videos\\album\\0.json").expect("File not found");
+    let file = File::open(opt.jfile).expect("File not found");
     let  album: Album = serde_json::from_reader(file).expect("JSON was not well-formatted");
 
-    for photo in &album.photos {
-        let timestamp = photo.creation_timestamp;
+    for photo in album.photos {
+        
+        if photo.media_metadata.photo_metadata.taken_timestamp > 0 {
+            let timestamp = photo.media_metadata.photo_metadata.taken_timestamp;            
+            let d = UNIX_EPOCH + Duration::from_secs(timestamp);
+            let datetime = DateTime::<Local>::from(d);
+            
+            let timestamp_str = datetime.format("%Y-%m-%d %H:%M:%S").to_string();  
+            
+            cmd_line.push_str(&format!("\"-EXIF:DateTimeOriginal={}\" \"-EXIF:Create:Date={}\"", timestamp_str, timestamp_str));
+        }
 
-        // Creates a new SystemTime from the specified number of whole seconds
-        let d = UNIX_EPOCH + Duration::from_secs(timestamp);
-        // Create DateTime from SystemTime
-        let datetime = DateTime::<Local>::from(d);
-        // Formats the combined date and time with the specified format string.
-        let timestamp_str = datetime.format("%Y-%m-%d %H:%M:%S").to_string();
+        if photo.media_metadata.photo_metadata.modified_timestamp > 0 {
+            let timestamp = photo.media_metadata.photo_metadata.modified_timestamp;            
+            let d = UNIX_EPOCH + Duration::from_secs(timestamp);
+            let datetime = DateTime::<Local>::from(d);
+            
+            let timestamp_str = datetime.format("%Y-%m-%d %H:%M:%S").to_string();  
+            
+            cmd_line.push_str(&format!("\"-EXIF:DateTimeOriginal={}\" \"-EXIF:Create:Date={}\"", timestamp_str, timestamp_str));
+        }
 
-        println!("-CreateDate=\"{}\" {}", timestamp_str, photo.uri);    
+        println!("Camera Make: {}", photo.media_metadata.photo_metadata.camera_make);
+
+         
     }
 
-    println!("\n\nLista: \n {:?}", album);
+    //println!("{}", cmd_line);   
+    //println!("\n\nLista: \n {:?}", album);
     
 
 }
